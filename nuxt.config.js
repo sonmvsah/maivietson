29f3e415d2a4d6eeba350d37f89b8badc9a75345
sonmvsah <sonmv.sah@gmail.com>
2023-02-26 05:24:11 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,15 +20,21 @@ export default {
       },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "preconnect", type: "", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect", type: "", href: "https://fonts.gstatic.com" },
+      {
+        rel: "stylesheet",
+        type: "",
+        href: "https://fonts.googleapis.com/css2?family=Fahkwang:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
+      },
+    ],
+    css: ["~/assets/css/custom.css"],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "@/assets/css/main.css",
-    "@/static/css/main.css",
-    "~/assets/css/custom.css",
-  ],
+  css: ["~/assets/css/main.css", "@/static/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
