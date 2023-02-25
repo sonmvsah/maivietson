ba575bd012fdba7cb44c50d6b1d0c6da2f65702a
sonmvsah <sonmv.sah@gmail.com>
2023-02-26 05:00:35 +0700

    update custom css

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/css/main.css", "@/static/css/main.css"],
+  css: [
+    "@/assets/css/main.css",
+    "@/static/css/main.css",
+    "@/assets/css/custom.css",
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
